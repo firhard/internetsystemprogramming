de91c3808e2f89fbb4c9fd25978d02adb9ee1b33
Jonathan Lee <jonathan_lee7@live.com>
2017-02-01 18:24:33 -0600

    Removing log

--- Assignment1/WebContent/passWordInputValidation.js
+++ Assignment1/WebContent/passWordInputValidation.js
@@ -5,10 +5,7 @@
 window.onsubmit = function(){
 	let pass1 = document.getElementById('pass1').value
 	
-	let pass2 = document.getElementById('pass2').value
-	
-	console.log("fuck this");
-	
+	let pass2 = document.getElementById('pass2').value	
 	
 	if(pass1 == pass2){
 		return true;
