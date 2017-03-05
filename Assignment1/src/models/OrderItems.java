26732e675188df26a6d2a8fa22aa4a5adf8e2771
Jonathan Lee <jonathan_lee7@live.com>
2017-03-05 17:40:11 -0600

    OrderItems creation

--- Assignment1/src/models/OrderItems.java
+++ Assignment1/src/models/OrderItems.java
@@ -19,4 +19,66 @@ public class OrderItems {
 	public int getId() {
 		return Id;
 	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public int getOrderId() {
+		return OrderId;
+	}
+
+	public void setOrderId(int orderId) {
+		OrderId = orderId;
+	}
+
+	public int getProductId() {
+		return ProductId;
+	}
+
+	public void setProductId(int productId) {
+		ProductId = productId;
+	}
+
+	public int getProductPrice() {
+		return ProductPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		ProductPrice = productPrice;
+	}
+
+	public int getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+
+	public byte getShippingStatus() {
+		return ShippingStatus;
+	}
+
+	public void setShippingStatus(byte shippingStatus) {
+		ShippingStatus = shippingStatus;
+	}
+
+	public int getShippingRefNo() {
+		return ShippingRefNo;
+	}
+
+	public void setShippingRefNo(int shippingRefNo) {
+		ShippingRefNo = shippingRefNo;
+	}
+
+	public byte getStatus() {
+		return Status;
+	}
+
+	public void setStatus(byte status) {
+		Status = status;
+	}
+
+
 }
